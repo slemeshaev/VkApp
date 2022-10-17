c45b50c46338eafba010213d31aa69b9354274d1
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-10-17 22:24:09 +0300

    Initial project

---
+++ VkApp/Screens/SignIn/SignInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewController.swift
+//  VkApp
+//
+//  Created by Stanislav Lemeshaev on 17.10.2022.
+//  Copyright © 2022 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
